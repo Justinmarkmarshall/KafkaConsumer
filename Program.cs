ea1c40776e34f9cab7e6d29f5cdfc304d7ed70ef
Justin Marshall <justinmarkmarshall1@gmail.com>
2022-07-30 14:19:43 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+var config = new ConsumerConfig
+{
+    GroupId = "weather-consumer-group",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+
+consumer.Subscribe("weather-topic");
+
+CancellationTokenSource token = new();
+
+try
+{
+    while (true)
+    {
+        var response = consumer.Consume(token.Token);
+        if (response.Message != null)
+        {
+            var weather = JsonConvert.DeserializeObject<Weather>(response.Message.Value);
+            Console.WriteLine($"State: {weather.State}, Temperature: {weather.Temp}");
+        }
+    }
+}
+catch(ConsumeException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+   public record Weather
+{
+    public string State { get; set; }
+    public int Temp { get; set; }
+}
